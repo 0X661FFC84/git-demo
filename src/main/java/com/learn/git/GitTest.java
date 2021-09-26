23c7f77dad7327541e0ddb912a63699603d14145
wangzijian <1713372292@qq.com>
2021-09-26 15:55:18 +0800

    my idea second commit

--- src/main/java/com/learn/git/GitTest.java
+++ src/main/java/com/learn/git/GitTest.java
@@ -3,5 +3,6 @@ package com.learn.git;
 public class GitTest {
     public static void main(String[] args) {
         // \u000d System.out.println("wangzijian");
+        // \u000d System.out.println("wangzijian");
     }
 }
